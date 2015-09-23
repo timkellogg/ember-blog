edc006749054630f0a52ebbd27c85e9e21bd69cb
timkellogg <tim.kellogg@gmail.com>
2015-09-22 23:49:09 -0700

    finish hardcoded version

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,5 +1,22 @@
 import Ember from 'ember';
 
-export default Ember.Route.extend({
+	var articles = [
+		{
+		  title: "Times in the Rain",
+			categories: ['programming', 'weather'],
+			body: 'This is the body of the first article',
+			comments: ''
+		}, 
+		{
+		  title: "The cool kids ran away",
+		  categories: ['awesome', 'posum'],
+		  body: 'This is the body of the second article',
+		  comments: ''
+		}
+	];
 
+export default Ember.Route.extend({
+	model() {
+		return articles;
+	}
 });
